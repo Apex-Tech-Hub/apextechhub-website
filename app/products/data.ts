22dfb549988073a7d8799df01cfd3e5a4b0d3f53
ibrarli <ibraralihaidar@gmail.com>
2026-01-21 15:02:36 +0500

    Update batch v0.0.8

---
+++ app/products/data.ts
@@ -0,0 +1,12 @@
+export const portfolio = {
+  active: "Portfolio",
+  portfolio: [
+    {
+      id: 1,
+      pdfUrl: "/portfolio.pdf",
+      imageUrl: "/gis4.jpg",
+      title: "View Our Work",
+      description: "Check out our portfolio"
+    },
+  ]
+};
